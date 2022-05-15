7393c1932c670f3fe912ca12da41743aaccc5070
Dennis von Bargen <dennis@vonbargen.se>
2022-05-15 17:27:26 +0200

    Added integration tests for DI resolution

---
+++ MailQ.Core.Tests/DependencyInjection/DependencyInjectionTests.cs
@@ -0,0 +1,27 @@
+using Autofac;
+using Autofac.Core;
+using MailQ.Core.DependencyInjection;
+
+namespace MailQ.Core.Tests.DependencyInjection;
+
+public class DependencyInjectionTests
+{
+    [Fact]
+    public void Load_Any_ResolvesAllRegisteredDependencies()
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterModule<EmailerModule>();
+        
+        var container = builder.Build();
+        using var scope = container.BeginLifetimeScope();
+
+        var distinctTypes = scope.ComponentRegistry.Registrations
+            .SelectMany(r => r.Services.OfType<TypedService>().Select(s => s.ServiceType))
+            .Distinct();
+
+        foreach (var distinctType in distinctTypes)
+        {
+            container.Resolve(distinctType);
+        }
+    }
+}
