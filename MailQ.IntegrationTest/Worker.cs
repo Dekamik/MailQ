26fd8107d44694c5592dd9f0fa21b3905813f433
Dennis von Bargen <dennis.von-bargen@viati.se>
2022-05-24 16:54:33 +0200

    Added integration test docker image

---
+++ MailQ.IntegrationTest/Worker.cs
@@ -0,0 +1,50 @@
+using Google.Protobuf;
+using MailQ.Protobuf;
+using RabbitMQ.Client;
+
+namespace MailQ.IntegrationTest;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var subject = Environment.GetEnvironmentVariable("EMAIL_SUBJECT")!;
+        var body = Environment.GetEnvironmentVariable("EMAIL_BODY")!;
+        var to = Environment.GetEnvironmentVariable("EMAIL_TO")!.Split(",");
+        
+        var rabbitmqConnectionString = Environment.GetEnvironmentVariable("RABBITMQ_CONNECTION_STRING")!;
+        var exchange = Environment.GetEnvironmentVariable("RABBITMQ_EXCHANGE");
+        var queue = Environment.GetEnvironmentVariable("RABBITMQ_QUEUE");
+        var routingKey = Environment.GetEnvironmentVariable("RABBITMQ_ROUTING_KEY");
+
+        var connectionFactory = new ConnectionFactory
+        {
+            Uri = new Uri(rabbitmqConnectionString)
+        };
+
+        using var connection = connectionFactory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue, false, false, false, null);
+        
+        _logger.LogInformation("Connection established");
+
+        var mail = new Mail
+        {
+            Subject = subject,
+            Body = body,
+            To = { to }
+        };
+        var buffer = mail.ToByteArray();
+        
+        channel.BasicPublish(exchange, routingKey, null, buffer);
+        
+        _logger.LogInformation("Mail published!");
+    }
+}
